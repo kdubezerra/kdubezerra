090f52c37c481b6c7a70fceaff7675327c40f77e
kdubezerra <kdubezerra@67108f60-033f-0410-b9a3-03c69097e409>
2011-02-18 18:15:38 +0000

    it's alive...

---
+++ libcosmmus-msg/layer_core/src/PaxosLearnerInterface.cpp
@@ -0,0 +1,21 @@
+/*
+ * PaxosLearner.cpp
+ *
+ *  Created on: 15/02/2011
+ *      Author: Carlos Eduardo B. Bezerra - carlos.bezerra@usi.ch
+ */
+
+#include "../include/PaxosLearnerInterface.h"
+
+namespace optpaxos {
+
+PaxosLearnerInterface::PaxosLearnerInterface() {
+  // TODO Auto-generated constructor stub
+
+}
+
+PaxosLearnerInterface::~PaxosLearnerInterface() {
+  // TODO Auto-generated destructor stub
+}
+
+}
